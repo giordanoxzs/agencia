843f2e126ba560e8e62b09d9eeb9f4cfd746d118
giordanoxzs <138598314+giordanoxzs@users.noreply.github.com>
2023-08-29 12:02:27 -0300

    criar metodo de calcular idade do candidato e outro metodo para validar se e maior ou nao de idade (#1)

--- src/main/java/br/com/senai/models/Candidato.java
+++ src/main/java/br/com/senai/models/Candidato.java
@@ -1,6 +1,7 @@
 package br.com.senai.models;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,4 +17,14 @@ public class Candidato {
     public String nacionalidade;
     public Endereco endereco;
     public List<Escolaridade> escolaridades = new ArrayList<>();
-}
+
+    public int calcularIdade () {
+        return  Period.between(this.dataNascimento,
+            LocalDate.now()).getYears();
+    }
+// Preciso de um metodo para validar o candidato sendo maior de 18
+
+        public String eMaiorDeIdade () {
+         return calcularIdade()>= 18 ? "Sim" : "Não ";
+        }
+    }
